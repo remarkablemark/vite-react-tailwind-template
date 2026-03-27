05826364992893b164164ea4cd91dc570e2ba90d
Mark <mark@remarkablemark.org>
2026-03-26 22:24:43 -0400

    refactor(vite): tidy vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,56 +1,10 @@
 /// <reference types="vite/client" />
 
-declare module '*.avif' {
-  const src: string;
-  export default src;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ImportMetaEnv {
+  // readonly VITE_YOUR_ENV_VAR: string;
 }
 
-declare module '*.bmp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.gif' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpeg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.webp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.svg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.ogg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.mp3' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.wav' {
-  const src: string;
-  export default src;
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
